81dff2b08257a56c51763216cb318e8b67ff9ba8
Kadir Aslan <kdr.aslan@yahoo.com>
2020-04-24 23:25:28 +0300

    Printing 'ID's of pedestrians. First attempt for collusion prevention.

--- classes/cms.py
+++ classes/cms.py
@@ -15,6 +15,7 @@ class CMS(tk.Canvas):
         self.step = 0
         self.current_step_text = None
         self.control_button = None
+        self.utility = []  # Utility matrix that takes into account also the positions of the pedestrians.
 
         self.width = 600
         self.height = 600
@@ -75,7 +76,7 @@ class CMS(tk.Canvas):
             self.step = self.step + 1
             self.current_step_text.set(f"Current Step: {self.step}")
             self.draw()
-            self.after(100, self.loop)
+            self.after(500, self.loop)
 
     # Draws the canvas.
     def draw(self):
@@ -96,19 +97,23 @@ class CMS(tk.Canvas):
         return self.rect_start_x + x * self.cell_size, self.rect_start_y + y * self.cell_size
 
     # Fills the cells.
-    def fill(self, x, y, color):
+    def fill(self, x, y, color, i):
         self.create_rectangle(x + self.offset['D'],
                               y + self.offset['D'],
                               x + self.cell_size - self.offset['D'],
                               y + self.cell_size - self.offset['D'],
                               fill=color)
+        self.create_text(x + self.offset['D'] + self.cell_size/2,
+                         y + self.offset['D'] + self.cell_size/2,
+                         fill="#FFFFFF",
+                         text=i)
 
     # Evaluates the next state of the system.
     def evaluate(self):
         # Rendering Obstacles.
         for obstacle in self.simulation_grid.elements['O']:
             coord_x, coord_y = self.coordinate(*obstacle.current_pos)
-            self.fill(coord_x, coord_y, obstacle.color)
+            self.fill(coord_x, coord_y, obstacle.color, "O")
 
         # Check success condition
         self.success = True
@@ -118,7 +123,7 @@ class CMS(tk.Canvas):
                 break
 
         # Rendering Pedestrians.
-        for pedestrian in self.simulation_grid.elements['P']:
+        for i, pedestrian in enumerate(self.simulation_grid.elements['P']):
             if not pedestrian.has_arrived:
                 distance, move_target = self.get_move_coordinate(pedestrian)
                 pedestrian.move(move_target)
@@ -126,11 +131,11 @@ class CMS(tk.Canvas):
                     pedestrian.has_arrived = True
 
             coord_x, coord_y = self.coordinate(*pedestrian.current_pos)
-            self.fill(coord_x, coord_y, pedestrian.color)
+            self.fill(coord_x, coord_y, pedestrian.color, str(i+1))
 
         # Rendering Target.
         coord_x, coord_y = self.coordinate(*self.simulation_grid.elements['T'].current_pos)
-        self.fill(coord_x, coord_y, self.simulation_grid.elements['T'].color)
+        self.fill(coord_x, coord_y, self.simulation_grid.elements['T'].color, "T")
 
         # Check if the simulation reached the desired state.
         if self.success:
@@ -145,11 +150,20 @@ class CMS(tk.Canvas):
         current_selected_neighbor = {}
         current_min_distance = float("inf")
 
+        for d in self.simulation_grid.distance_field:
+            self.utility.append(d[:])
+
+        for pedestrian in self.simulation_grid.elements['P']:
+            if not pedestrian.has_arrived:
+                self.utility[pedestrian.current_pos[0]][pedestrian.current_pos[1]] = float("inf")
+
         for neighbor in neighbors:
             col, row = neighbor
-            distance = self.simulation_grid.distance_field[row][col]
+            distance = self.utility[row][col]
             if distance < current_min_distance:
                 current_min_distance = distance
                 current_selected_neighbor = neighbor
+
+        self.utility = []
 
         return current_min_distance, current_selected_neighbor
